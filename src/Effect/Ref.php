9a2110b9b12a17835a91f1791cd1a9323807fdfb
0x1 <kevin.francart3@gmail.com>
2026-07-05 20:08:35 +0200

    perf(ffi): replace phpurs_uncurry overhead with inline uncurrying closures

--- src/Effect/Ref.php
+++ src/Effect/Ref.php
@@ -1,54 +1,24 @@
 <?php
-if (!function_exists('phpurs_uncurry2')) {
-function phpurs_uncurry2($fn) {
-    return function($a, $b = null) use ($fn) {
-        if (func_num_args() < 2) {
-            $__args = func_get_args();
-            return function(...$more) use ($fn, $__args) {
-                return phpurs_uncurry2($fn)(...array_merge($__args, $more));
-            };
-        }
-        return $fn($a)($b);
-    };
-}
-function phpurs_uncurry3($fn) {
-    return function($a, $b = null, $c = null) use ($fn) {
-        if (func_num_args() < 3) {
-            $__args = func_get_args();
-            return function(...$more) use ($fn, $__args) {
-                return phpurs_uncurry3($fn)(...array_merge($__args, $more));
-            };
-        }
-        return $fn($a)($b)($c);
-    };
-}
-function phpurs_uncurry4($fn) {
-    return function($a, $b = null, $c = null, $d = null) use ($fn) {
-        if (func_num_args() < 4) {
-            $__args = func_get_args();
-            return function(...$more) use ($fn, $__args) {
-                return phpurs_uncurry4($fn)(...array_merge($__args, $more));
-            };
-        }
-        return $fn($a)($b)($c)($d);
-    };
-}
-function phpurs_uncurry5($fn) {
-    return function($a, $b = null, $c = null, $d = null, $e = null) use ($fn) {
-        if (func_num_args() < 5) {
-            $__args = func_get_args();
-            return function(...$more) use ($fn, $__args) {
-                return phpurs_uncurry5($fn)(...array_merge($__args, $more));
-            };
-        }
-        return $fn($a)($b)($c)($d)($e);
-    };
-}
-}
-
-
 
 $Effect_Ref__new = function($val) { return function() use(&$val) { return (object)['value' => $val]; }; };
 $Effect_Ref_read = function($ref) { return function() use(&$ref) { return $ref->value; }; };
-$Effect_Ref_modifyImpl = phpurs_uncurry2(function($f) { return function($ref) use(&$f) { return function() use(&$f, &$ref) { $t = $f($ref->value); $ref->value = $t->state; return $t->value; }; }; });
-$Effect_Ref_write = phpurs_uncurry2(function($val) { return function($ref) use(&$val) { return function() use(&$val, &$ref) { $ref->value = $val; return null; }; }; });
+$Effect_Ref_modifyImpl = function($f, $ref = null) {
+    if (func_num_args() < 2) {
+        $__args = func_get_args();
+        return function(...$more) use ($__args) {
+            global $Effect_Ref_modifyImpl;
+            return $Effect_Ref_modifyImpl(...array_merge($__args, $more));
+        };
+    }
+    return function() use(&$f, &$ref) { $t = $f($ref->value); $ref->value = $t->state; return $t->value; };
+};
+$Effect_Ref_write = function($val, $ref = null) {
+    if (func_num_args() < 2) {
+        $__args = func_get_args();
+        return function(...$more) use ($__args) {
+            global $Effect_Ref_write;
+            return $Effect_Ref_write(...array_merge($__args, $more));
+        };
+    }
+    return function() use(&$val, &$ref) { $ref->value = $val; return null; };
+};

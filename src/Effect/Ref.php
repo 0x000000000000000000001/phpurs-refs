ba93586c6435a97dfcc7d0e3fe4be13abc42c519
0x1 <kevin.francart3@gmail.com>
2026-07-06 19:17:10 +0200

    feat: migrate FFI to exports array format

--- src/Effect/Ref.php
+++ src/Effect/Ref.php
@@ -1,24 +1,30 @@
 <?php
 
-$Effect_Ref__new = function($val) { return function() use(&$val) { return (object)['value' => $val]; }; };
-$Effect_Ref_read = function($ref) { return function() use(&$ref) { return $ref->value; }; };
-$Effect_Ref_modifyImpl = function($f, $ref = null) {
+$_new = function($val) use (&$_new) { return function() use(&$val) { return (object)['value' => $val]; }; };
+$read = function($ref) use (&$read) { return function() use(&$ref) { return $ref->value; }; };
+$modifyImpl = function($f, $ref = null) use (&$modifyImpl) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Effect_Ref_modifyImpl;
-            return $Effect_Ref_modifyImpl(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$_new) {
+
+            return $modifyImpl(...array_merge($__args, $more));
         };
     }
     return function() use(&$f, &$ref) { $t = $f($ref->value); $ref->value = $t->state; return $t->value; };
 };
-$Effect_Ref_write = function($val, $ref = null) {
+$write = function($val, $ref = null) use (&$write) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Effect_Ref_write;
-            return $Effect_Ref_write(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$read) {
+
+            return $write(...array_merge($__args, $more));
         };
     }
     return function() use(&$val, &$ref) { $ref->value = $val; return null; };
 };
+
+$exports['_new'] = $_new;
+$exports['read'] = $read;
+$exports['modifyImpl'] = $modifyImpl;
+$exports['write'] = $write;
+return $exports;

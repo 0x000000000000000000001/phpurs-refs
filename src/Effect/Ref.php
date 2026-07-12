9e907ea893b28f7ad4c483fd3c075c5b296539fb
0x1 <kevin.francart3@gmail.com>
2026-07-12 17:39:48 +0200

    [tmp] add slash to native PHP functions for OPCache

--- src/Effect/Ref.php
+++ src/Effect/Ref.php
@@ -3,21 +3,21 @@
 $_new = function($val) use (&$_new) { return function() use(&$val) { return (object)['value' => $val]; }; };
 $read = function($ref) use (&$read) { return function() use(&$ref) { return $ref->value; }; };
 $modifyImpl = function($f, $ref = null) use (&$modifyImpl) {
-    if (func_num_args() < 2) {
-        $__args = func_get_args();
+    if (\func_num_args() < 2) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$modifyImpl) {
 
-            return $modifyImpl(...array_merge($__args, $more));
+            return $modifyImpl(...\array_merge($__args, $more));
         };
     }
     return function() use(&$f, &$ref) { $t = $f($ref->value); $ref->value = $t->state; return $t->value; };
 };
 $write = function($val, $ref = null) use (&$write) {
-    if (func_num_args() < 2) {
-        $__args = func_get_args();
+    if (\func_num_args() < 2) {
+        $__args = \func_get_args();
         return function(...$more) use ($__args, &$write) {
 
-            return $write(...array_merge($__args, $more));
+            return $write(...\array_merge($__args, $more));
         };
     }
     return function() use(&$val, &$ref) { $ref->value = $val; return null; };
